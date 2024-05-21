9607a55f2b770e848bdb6c6dc4c16b1303f4b8bc
julianzotelo <juli.zotelo@gmail.com>
2024-05-20 23:31:37 -0300

    cs

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,15 +1,56 @@
 extends CharacterBody2D
 
-@export var speed : float
 
-func _process(delta):
-	motion_ctrl()
-	if not Input.is_action_pressed("ui_accept"):
-		$PJ/AnimationPlayer.play("asd")
+const MoveSpeed = 250
+const MaxSpeed = 50
+const JUMP_VELOCITY = -400
+
+const up = Vector2(0,-1)
+#const gravity = 15
 
 
-func motion_ctrl() -> void:
-	velocity.x = Global.get_axis().x * speed
-	velocity.y = Global.get_axis().y * -speed
+var motion = Vector2()
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+
+	motion.y -= gravity
+	velocity.y += gravity * delta*0.7
+	
+	if  is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+		$PJ/Anim.play("asd")
+
+		
+	if  Input.is_action_just_pressed("ui_left"):
+		$PJ.flip_h = true
+	elif  Input.is_action_just_pressed("ui_right"):
+		$PJ.flip_h = false
+		
+		
+		
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * MoveSpeed
+	else:
+		velocity.x = move_toward(velocity.x, 0, MoveSpeed)
+
 	move_and_slide()
+	#animaciones()
 
+#func animaciones():
+	#if is_on_floor():
+		#if velocity.x != 0:
+			#$Anim.play("idle")
+		#else:
+			#$Anim.play("idle")
+	#else:
+		#$Anim.play("Flip")
+
+
+
+func _on_animation_player_animation_finished(anim_name):
+	$PJ/Anim.play("idle")
